f7aec7be0cba4d8920f1c269ccbb786a2da11a90
Roberto Stepic <roberto.stepic@gmail.com>
2023-11-05 22:40:23 +0100

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://rstepic:Vr6Su1hikl8a@cluster0.tobvvrl.mongodb.net/todo?retryWrites=true&w=majority").then(()=>{
+mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("Database connected")
 }).catch((err)=>{
     console.log(err)
